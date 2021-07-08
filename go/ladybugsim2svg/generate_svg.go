f7391c3be2d8b6e14163789b2edf5c1fff298e43
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2021-07-08 13:09:47 +0200

    smootheer

--- go/ladybugsim2svg/generate_svg.go
+++ go/ladybugsim2svg/generate_svg.go
@@ -16,35 +16,42 @@ type ladybugsimToSVGTranformer struct {
 
 var LadybugsimToSVGTranformerSingloton ladybugsimToSVGTranformer
 
+var svgSingloton *gongsvg_models.SVG
+var fenceSingloton *gongsvg_models.Rect
+
+var mapLadybug_Circle = make(map[*ladybugsim_models.Ladybug]*gongsvg_models.Circle)
+var mapLadybug_Animate = make(map[*ladybugsim_models.Ladybug]*gongsvg_models.Animate)
+
 // callback on the commit function
 func (ladybugsimToSVGTranformer *ladybugsimToSVGTranformer) BeforeCommit(stage *ladybugsim_models.StageStruct) {
 
 	// remove all gongsvg stage/repo
 	gongsvg_models.Stage.Checkout()
-	gongsvg_models.Stage.Reset()
-	gongsvg_models.Stage.Commit()
 	ladybugsim_models.Stage.Checkout()
 
 	//
 	// SVG
 	//
-	svg := new(gongsvg_models.SVG).Stage()
-	svg.Name = "New ladybugsim Chart"
-	svg.Display = true
+	if svgSingloton == nil {
+		svgSingloton = new(gongsvg_models.SVG).Stage()
+		svgSingloton.Name = "New ladybugsim Chart"
+		svgSingloton.Display = true
+	}
 
-	fence := new(gongsvg_models.Rect).Stage()
-	fence.Name = "fence"
-	svg.Rects = append(svg.Rects, fence)
+	if fenceSingloton == nil {
+		fenceSingloton = new(gongsvg_models.Rect).Stage()
+		fenceSingloton.Name = "fence"
+		svgSingloton.Rects = append(svgSingloton.Rects, fenceSingloton)
+		fenceSingloton.X = 50
+		fenceSingloton.Y = 50
+		fenceSingloton.Height = 30
+		fenceSingloton.Width = 2000
 
-	fence.X = 50
-	fence.Y = 50
-	fence.Height = 30
-	fence.Width = 2000
-
-	fence.Color = "blue"
-	fence.FillOpacity = 0.1
-	fence.Stroke = "blue"
-	fence.StrokeWidth = 0.5
+		fenceSingloton.Color = "blue"
+		fenceSingloton.FillOpacity = 0.1
+		fenceSingloton.Stroke = "blue"
+		fenceSingloton.StrokeWidth = 0.5
+	}
 
 	simSpeed := gongsim_models.EngineSingloton.Speed
 
@@ -54,9 +61,18 @@ func (ladybugsimToSVGTranformer *ladybugsimToSVGTranformer) BeforeCommit(stage *
 	}
 
 	for _, ladybug := range sim.Ladybugs {
-		circle := new(gongsvg_models.Circle).Stage()
-		circle.Name = ladybug.Name
-		svg.Circles = append(svg.Circles, circle)
+
+		circle := mapLadybug_Circle[ladybug]
+		if circle == nil {
+			circle = new(gongsvg_models.Circle).Stage()
+			circle.Name = ladybug.Name
+			circle.FillOpacity = 0.1
+			circle.StrokeWidth = 0.5
+			circle.Radius = 5
+
+			svgSingloton.Circles = append(svgSingloton.Circles, circle)
+			mapLadybug_Circle[ladybug] = circle
+		}
 
 		if ladybug.Speed > 0 {
 			circle.Color = "red"
@@ -65,83 +81,95 @@ func (ladybugsimToSVGTranformer *ladybugsimToSVGTranformer) BeforeCommit(stage *
 			circle.Color = "green"
 			circle.Stroke = "green"
 		}
-		circle.FillOpacity = 0.1
-		circle.StrokeWidth = 0.5
-		circle.CX = fence.X + ladybug.Position*fence.Width
-		circle.CY = fence.Y
-		circle.Radius = 5
+		circle.CX = fenceSingloton.X + ladybug.Position*fenceSingloton.Width
+		circle.CY = fenceSingloton.Y
 
 		if ladybug.LadybugStatus == ladybugsim_models.ON_THE_GROUND {
-			circle.CY = fence.Y + fence.Height
+			circle.CY = fenceSingloton.Y + fenceSingloton.Height
 		}
 
-		if ladybug.LadybugStatus == ladybugsim_models.ON_THE_FENCE {
-			// add animation
-			animate := new(gongsvg_models.Animate).Stage()
+		animate := mapLadybug_Animate[ladybug]
+		if animate == nil {
+			animate = new(gongsvg_models.Animate).Stage()
 			animate.Name = ladybug.Name
 			circle.Animations = append(circle.Animations, animate)
 			animate.AttributeName = "cx"
+			mapLadybug_Animate[ladybug] = animate
+		}
 
-			animate.Values = fmt.Sprintf("%d;%d",
-				int64(circle.CX),
-				int64(circle.CX+
-					simSpeed*
-						ladybug.Speed*
-						fence.Width))
+		//  gongsim_models.EngineSingloton.State == gongsim_models.RUNNING
+		if ladybug.LadybugStatus == ladybugsim_models.ON_THE_FENCE {
+			animate.Stage()
+
+			if gongsim_models.EngineSingloton.State == gongsim_models.RUNNING {
+				animate.Values = fmt.Sprintf("%d;%d",
+					int64(circle.CX),
+					int64(circle.CX+
+						simSpeed*
+							ladybug.Speed*
+							fenceSingloton.Width))
+			} else {
+				animate.Values = fmt.Sprintf("%d;%d",
+					int64(circle.CX),
+					int64(circle.CX))
+			}
 			animate.Dur = "1s"
 			animate.RepeatCount = "undefinite"
+		} else {
+			animate.Unstage()
+			circle.Animations = nil
 		}
 
 	}
 
-	{
-		circle := new(gongsvg_models.Circle).Stage()
-		circle.Name = "laft relay"
-		svg.Circles = append(svg.Circles, circle)
+	// {
+	// 	circle := new(gongsvg_models.Circle).Stage()
+	// 	circle.Name = "laft relay"
+	// 	svgSingloton.Circles = append(svgSingloton.Circles, circle)
 
-		circle.Color = "black"
-		circle.Stroke = "black"
+	// 	circle.Color = "black"
+	// 	circle.Stroke = "black"
 
-		circle.FillOpacity = 0.1
-		circle.StrokeWidth = 0.5
-		circle.CX = fence.X + sim.LeftRelayInitialPosX*fence.Width
-		circle.CY = fence.Y
-		circle.Radius = 10
+	// 	circle.FillOpacity = 0.1
+	// 	circle.StrokeWidth = 0.5
+	// 	circle.CX = fenceSingloton.X + sim.LeftRelayInitialPosX*fenceSingloton.Width
+	// 	circle.CY = fenceSingloton.Y
+	// 	circle.Radius = 10
 
-		// add animation
-		animate := new(gongsvg_models.Animate).Stage()
-		animate.Name = circle.Name
-		circle.Animations = append(circle.Animations, animate)
-		animate.AttributeName = "cx"
-		animate.Values = fmt.Sprintf("%d;%d", int64(circle.CX),
-			int64(circle.CX+simSpeed*sim.AbsoluteSpeed*fence.Width))
-		animate.Dur = "1s"
-		animate.RepeatCount = "undefinite"
-	}
+	// 	// add animation
+	// 	animate := new(gongsvg_models.Animate).Stage()
+	// 	animate.Name = circle.Name
+	// 	circle.Animations = append(circle.Animations, animate)
+	// 	animate.AttributeName = "cx"
+	// 	animate.Values = fmt.Sprintf("%d;%d", int64(circle.CX),
+	// 		int64(circle.CX+simSpeed*sim.AbsoluteSpeed*fenceSingloton.Width))
+	// 	animate.Dur = "1s"
+	// 	animate.RepeatCount = "undefinite"
+	// }
 
-	{
-		circle := new(gongsvg_models.Circle).Stage()
-		circle.Name = "laft relay"
-		svg.Circles = append(svg.Circles, circle)
+	// {
+	// 	circle := new(gongsvg_models.Circle).Stage()
+	// 	circle.Name = "laft relay"
+	// 	svgSingloton.Circles = append(svgSingloton.Circles, circle)
 
-		circle.Color = "black"
-		circle.Stroke = "black"
+	// 	circle.Color = "black"
+	// 	circle.Stroke = "black"
 
-		circle.FillOpacity = 0.1
-		circle.StrokeWidth = 0.5
-		circle.CX = fence.X + sim.RightRelayInitialPosX*fence.Width
-		circle.CY = fence.Y
-		circle.Radius = 10
+	// 	circle.FillOpacity = 0.1
+	// 	circle.StrokeWidth = 0.5
+	// 	circle.CX = fenceSingloton.X + sim.RightRelayInitialPosX*fenceSingloton.Width
+	// 	circle.CY = fenceSingloton.Y
+	// 	circle.Radius = 10
 
-		// add animation
-		animate := new(gongsvg_models.Animate).Stage()
-		animate.Name = circle.Name
-		circle.Animations = append(circle.Animations, animate)
-		animate.AttributeName = "cx"
-		animate.Values = fmt.Sprintf("%d;%d", int64(circle.CX), int64(circle.CX-simSpeed*sim.AbsoluteSpeed*fence.Width))
-		animate.Dur = "1s"
-		animate.RepeatCount = "undefinite"
-	}
+	// 	// add animation
+	// 	animate := new(gongsvg_models.Animate).Stage()
+	// 	animate.Name = circle.Name
+	// 	circle.Animations = append(circle.Animations, animate)
+	// 	animate.AttributeName = "cx"
+	// 	animate.Values = fmt.Sprintf("%d;%d", int64(circle.CX), int64(circle.CX-simSpeed*sim.AbsoluteSpeed*fenceSingloton.Width))
+	// 	animate.Dur = "1s"
+	// 	animate.RepeatCount = "undefinite"
+	// }
 
 	gongsvg_models.Stage.Commit()
 
