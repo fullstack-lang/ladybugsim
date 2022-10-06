6ac4acbd627f1976010739523ebee5840a024347
peugeot <thomaspeugeot@yahoo.fr>
2022-10-06 21:47:30 +0200

    new gongc

--- ng/projects/ladybugsim/src/lib/ladybug-detail/ladybug-detail.component.ts
+++ ng/projects/ladybugsim/src/lib/ladybug-detail/ladybug-detail.component.ts
@@ -1,6 +1,6 @@
 // generated from NgDetailTemplateTS
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { UntypedFormControl } from '@angular/forms';
 
 import { LadybugDB } from '../ladybug-db'
 import { LadybugService } from '../ladybug.service'
